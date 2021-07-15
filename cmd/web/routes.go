62be4877a6c545f36f65d935ce3526da352fbc72
mubeen <mubeenarman19@gmail.com>
2021-07-15 23:32:03 +0900

    introduce custom and noSurf middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,9 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
+	//mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
